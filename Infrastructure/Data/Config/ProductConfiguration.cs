e2f00c278ea2896779f30bcb9097b4f279628809
unknown <temitayoadesugba@gmail.com>
2021-08-20 04:57:44 +0100

    End of section 16

--- Infrastructure/Data/Config/ProductConfiguration.cs
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -2,7 +2,7 @@ using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Infrastructure.Data.Migrations.Config
+namespace Infrastructure.Data.Config
 {
     public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
